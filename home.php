e935b849435f7c03423ff27009df7a1f075b8929
Mridul03-ice <abdulwahab22400@gmail.com>
2023-07-12 23:43:59 +0600

    slide

--- home.php
+++ home.php
@@ -7,10 +7,9 @@
     <title>home</title>
 
     <!-- swiper css link -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css?v=<?=$version?>" />
     <!-- font awesome cdn link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css?v=<?=$version?>">
 
     <!-- custome css file link -->
 
@@ -42,6 +41,120 @@
 
 
 <!-- header section ends -->
+
+<section class="home">
+
+
+
+    <div class="swiper home-slider">
+
+
+        <div class="swiper-wrapper">
+
+
+            <div class="swiper-slide slide" id="bg">
+                <div class="content">
+                    <span>explore,discover,travel</span>
+                    <h3>travel arround the world</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide" id="bg1">
+                <div class="content">
+                    <span>explore,discover,travel</span>
+                    <h3>discover the new places</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide" id="bg2">
+                <div class="content">
+                    <span>explore,discover,travel</span>
+                    <h3>make your tour worthwhile</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+
+
+    </div>
+
+
+
+
+</section>
+
+<!-- home section starts -->
+
+
+
+
+
+
+
+<!-- home section ends -->
+
+
+
+<!-- services section starts -->
+
+
+<section class="services">
+
+<h1 class="heading-title"> our services </h1>
+
+
+<div class="box-container">
+
+<div class="box">
+    <img src="img/icon-1.png" alt="">
+    <h3>adventure</h3>
+</div>
+
+<div class="box">
+    <img src="img/icon-2.png" alt="">
+    <h3>tour guide</h3>
+</div>
+
+<div class="box">
+    <img src="img/icon-3.png" alt="">
+    <h3>trakking</h3>
+</div>
+
+<div class="box">
+    <img src="img/icon-4.png" alt="">
+    <h3>camp fire</h3>
+</div>
+
+<div class="box">
+    <img src="img/icon-5.png" alt="">
+    <h3>off road</h3>
+</div>
+
+<div class="box">
+    <img src="img/icon-6.png" alt="">
+    <h3>camping</h3>
+</div>
+
+</div>
+
+
+</section>
+
+
+<!-- services section ends -->
+
+
+
+
+
+
 
 
 
@@ -83,14 +196,14 @@
     <a href="#"> <i class="fas fa-phone"></i> +8801303-579271 </a>
     <a href="#"> <i class="fas fa-phone"></i> +8801750-861021 </a>
     <a href="#"> <i class="fas fa-envelope"></i> abdulwahab22400@gmail.com </a>
-    <a href="#"> <i class="fas fa-map"></i> BAUET,Kadirabad Contonment,Natore</a>
+    <a href="https://goo.gl/maps/tRq1ZELx4uAp8Qhq8"> <i class="fas fa-map"></i> BAUET,Kadirabad Contonment,Natore</a>
 </div>
 
 <div class="box">
     <h3>follow us</h3>
-    <a href="#"><i class="fab fa-facebook-f"></i> facebook</a>
-    <a href="#"><i class="fab fa-instagram"></i> instagram</a>
-    <a href="#"><i class="fab fa-linkedin"></i> facebook</a>
+    <a href="https://www.facebook.com/wahabmridul"><i class="fab fa-facebook-f"></i> facebook</a>
+    <a href="https://www.instagram.com/abdul_wahab_mridul/"><i class="fab fa-instagram"></i> instagram</a>
+    <a href="https://www.linkedin.com/in/md-abdul-wahab-0a88621a7/"><i class="fab fa-linkedin"></i> linkedin</a>
 </div>
 
 </div>
@@ -109,7 +222,6 @@
 
 <!-- swiper js link -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-
 
 <!-- custom js file link -->
 <script src="js/script.js"></script>
